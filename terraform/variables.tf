e782995958bfb87c88190d71156f6f2ef16df085
eshanmalyala <eshanmalyala@outlook.com>
2025-07-11 06:44:27 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  default = "sharedproejcet-1"
+}
+
+variable "region" {
+  default = "EU"
+}
+
+variable "bucket_name" {
+  default = "getwellsoon-bucket-11"
+}
+
+variable "pubsub_topic" {
+  default = "getwellsoon-topic-11"
+}
+
+variable "bq_dataset" {
+  default = "getwellsoon_dataset_today"
+}
+variable "table_name" {
+  default = "employe"
+}
